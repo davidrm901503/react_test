c1512e66e3461b2f6a08cec4183ae1dc7b463892
David <davidrm901503@gmail.com>
2021-10-25 17:46:17 -0400

    crytoanalyst-backend

---
+++ api/views.py
@@ -0,0 +1,26 @@
+from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse
+
+
+def coin_list(request):
+    from requests import Session
+    import json
+
+    url = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest'
+    parameters = {
+        'start': '1',
+        'limit': '100',
+        'convert': 'USD'
+    }
+    headers = {
+        'Accepts': 'application/json',
+        'X-CMC_PRO_API_KEY': '1746e747-6889-4cf3-98d7-f63e85740a11',
+    }
+
+    session = Session()
+    session.headers.update(headers)
+
+    response = session.get(url, params=parameters)
+    data = json.loads(response.text)
+    return JsonResponse(data['data'], safe=False)
+
